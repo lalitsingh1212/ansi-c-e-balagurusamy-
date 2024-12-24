b97b2c224d5ea5e7979e6be0af47b5fc8feb455b
lalitsingh1212 <lalitsingh5665@gmail.com>
2024-12-24 09:05:36 +0530

    Add files via upload

---
+++ 2_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    printf("5*1=%d\n",5*1);
+    printf("5*2=%d\n",5*2);
+    printf("5*3=%d\n",5*3);
+    printf("5*4=%d\n",5*4);
+    printf("5*5=%d\n",5*5);
+    printf("5*6=%d\n",5*6);
+    printf("5*7=%d\n",5*7);
+    printf("5*8=%d\n",5*8);
+    printf("5*9=%d\n",5*9);
+    printf("5*10=%d\n",5*10);
+
+    return 0;
+}
